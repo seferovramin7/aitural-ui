2f36a786b06f2c5e6fe0285d856ab62bd99abb9f
seferovramin7 <seferovramin7@gmail.com>
2025-05-08 08:37:35 +0400

    tag added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="app">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2JDXRGH"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    
     <header class="header">
       <div class="container header-container">
         <h1>AiTural</h1>
@@ -95,6 +100,13 @@ export default {
   created() {
     // Initialize chatId when the component is created
     this.chatId = this.generateChatId();
+    
+    // Google Tag Manager
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W2JDXRGH');
   },
   methods: {
     async sendMessage() {
